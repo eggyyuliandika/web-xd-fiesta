5330e8a84da75532ac36afb2c38a9182689e1f2f
eggyyuliandika <eggyyuliandika@gmail.com>
2024-06-22 15:31:37 +0800

    fisrt commit

---
+++ components/Blog/index.tsx
@@ -0,0 +1,27 @@
+import SectionTitle from "../Common/SectionTitle";
+import SingleBlog from "./SingleBlog";
+import blogData from "./blogData";
+
+const Blog = () => {
+  return (
+    <section id="blog" className="bg-primary/5 py-16 md:py-20 lg:py-28">
+      <div className="container">
+        <SectionTitle
+          title="Artist"
+          paragraph="In 2024, XD Fiesta comes with a new innovation, held over 2 days and featuring 6 national bands and 4 local bands."
+          center
+        />
+
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3">
+          {blogData.map((blog) => (
+            <div key={blog.id} className="w-full">
+              <SingleBlog blog={blog} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Blog;
